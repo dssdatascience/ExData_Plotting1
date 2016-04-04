d54d7b919d75a478933de10c89b8237592d66c6d
Doug Sparling <dsparling@amuniversal.com>
2016-04-03 19:23:12 -0500

    added plot3.R and plot3.png

---
+++ plot3.R
@@ -0,0 +1,28 @@
+# Read in the dataset as efficiently as possible
+inputData <- read.csv(file="household_power_consumption.txt", header=TRUE, sep=';',
+                      quote="\"", dec=".", fill=TRUE, na.strings="?",
+                      stringsAsFactors=FALSE, nrows=2075259, comment.char="")
+
+# Format the date
+inputData$Date <- as.Date(inputData$Date, format="%d/%m/%Y")
+
+# Subset the data: dates 2007-02-01 and 2007-02-02
+workingData <- subset(inputData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Clean up
+rm(inputData)
+
+# Convert dates/times
+workingData$Datetime <- as.POSIXct(paste(as.Date(workingData$Date), workingData$Time))
+
+# Generate plot3 and save as 480x480 PNG
+png("plot3.png", width=480, height=480)
+with(workingData, {
+    plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime, col='red')
+    lines(Sub_metering_3~Datetime, col='blue')
+})
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"))
+
+# Close the PNG device
+dev.off()
